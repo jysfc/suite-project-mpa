db2d6ce826229ca1ea0bf27fe8e830bf677c913f
jysfc <63255178+jysfc@users.noreply.github.com>
2020-11-17 00:51:45 -0800

    added pages, icon, img

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Result from "./components/pages/Result";
+import EditProperty from "./components/pages/EditProperty";
+import EditSuite from "./components/pages/EditSuite";
+import Login from "./components/pages/Login";
+import SelectProperty from "./components/pages/SelectProperty";
+import Suite from "./components/pages/Suite";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Result} />
+        <Route exact path="/create-answer" component={EditProperty} />
+        <Route exact path="/create-imagery" component={EditSuite} />
+        <Route exact path="/review-answer" component={Login} />
+        <Route exact path="/review-cue" component={SelectProperty} />
+        <Route exact path="/review-done" component={Suite} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
