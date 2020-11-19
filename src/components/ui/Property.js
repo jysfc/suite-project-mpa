8aa6fbc7f7545e889b5948c299c6fe9167d60793
jysfc <63255178+jysfc@users.noreply.github.com>
2020-11-19 01:28:19 -0800

    iterate select props

---
+++ src/components/ui/Property.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import RemoveIcon from "../../icons/remove.svg";
+
+export default function Property(props) {
+   return (
+      //<!-- Property -->
+      <div className="col mb-4 mr-2 shadow p-3 mb-5 bg-white rounded">
+         <Link
+            to="edit-property"
+            className="text-dark text-decoration-none"
+            type="button"
+         >
+            <h5>{props.property}</h5>
+         </Link>
+         <p> </p>
+
+         <Link to="" className="text-danger text-decoration-none float-right">
+            <img src={RemoveIcon} width="20px" id="hotel-add" alt="" />
+            Remove
+         </Link>
+      </div>
+   );
+}
