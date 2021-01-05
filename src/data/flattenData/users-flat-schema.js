429f90b4cc6179c539d8f71c183732c42fc4ac49
jysfc <yaoj415@gmail.com>
2021-01-05 01:05:38 -0800

    added flat user data

---
+++ src/data/flattenData/users-flat-schema.js
@@ -0,0 +1,10 @@
+const userFlattenSchema = {
+    id: string,
+    email: string,
+    password: string,
+    createdAt: number,
+    isActive: boolean,
+    propertyName: string,
+    propertyId: string,
+ };
+ 
---
+++ src/data/flattenData/users-flat.js
@@ -0,0 +1,30 @@
+export const users = [
+    {
+       id: "9bfbc757-fcc9-40c8-9f83-652fdefee41e",
+       email: "jay@mirage.com",
+       password: "5B6BAE31B3A9C341FC7AC920A037514F14F3C8C0",
+       createdAt: 1605664419064,
+       isActive: true,
+       propertyName: "The Mirage Hotel & Casino",
+       propertyId: "76e032b2-8ba6-4487-818c-5290cdbb5a1c"
+    },
+    {
+       id: "3e4f4858-88ba-47fc-b23d-0622f78588021",
+       email: "mike@mandalaybay.com",
+       password: "82CBE1D375C65A50EC24654B5272683FBB58B5B4",
+       createdAt: 1605664433241,
+       isActive: true,
+       propertyName: "Mandalay Bay Hotel & Casino",
+       propertyId: "96552e06-cdcc-43ee-9675-582d1381ca9b"
+    },
+    {
+       id: "49add96e-1c04-40d6-9256-231f314c0a2f",
+       email: "john@westinstfrancis.com",
+       password: "E9C1240161604E16A32DED612BA0C91B9B751FE0",
+       createdAt: 1606890505551,
+       isActive: true,
+       propertyName: "The Westin St. Francis",
+       propertyId: "21f77c6b-d814-4763-9d1c-d582f73ab457"
+    },
+ ];
+ 
